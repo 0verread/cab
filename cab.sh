df7ce9b8bd3f6a72677363b6c7aa7cba97463b72
sub0ne1 <iamsubhajit.d@gmail.com>
2024-05-24 11:19:11 -0400

    working

--- cab.sh
+++ cab.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-GREEN="#008000"
-
-TEXT_VENV_CREATE="creating virtual env"
-TEXT_VENV_ACTIVATE="activate virtual env"
-TEXT_INS_REQ="installing the requirements"
-
+# take a arg as name
 PROJECT_NAME=$(gum input --placeholder "project name?")
 
 if [ "$PROJECT_NAME" == "." ]
@@ -13,57 +8,16 @@ if [ "$PROJECT_NAME" == "." ]
         PROJECT_NAME="$(basename $PWD)"
 fi
 
-LIST_FILES=(
-    ".env"
-    ".gitignore"
-    "app.py"
-    "README.md"
-    "requirements.txt"
-    "__init__.py"
-    # config dir
-    "config/__init__.py"
-    "config/config.py"
-    "config/dev_config.py"
-    "config/production.py"
-    # controllers
-    "controllers/__init__.py"
-    # middlewares
-    "middlewares/__init__.py"
-    # models
-    "models/__init__.py"
-    "models/user_model.py"
-    # services
-    "services/__init__.py"
-    # routes and utils
-    "routes.py"
-    "utils.py"
-)
-
-# mkdir temp_dir
-# git clone -n --depth=1 --filter=tree:0 https://github.com/0verread/cab temp_dir
-# cd temp_dir
-# git sparse-checkout set --no-cone flask-app
-# git checkout
-# rm -rf .git
-# cd ..
-# cp -r temp_dir/ .
-# rm -rf temp_dir
-
 # if . entered as a project name, take the cur dir name and make that a project assuming you're trying to create project in curr dir
-echo "$PROJECT_NAME"
-echo "[ `date` ]": "$(gum style --foreground 212 START)"
-echo "[ `date` ]": "creating project $(gum style --foreground "#008000" "$PROJECT_NAME" )"
 
-mkdir -p -- "$PROJECT_NAME"
-mkdir -p -- "temp_dir"
-git clone -n --depth=1 --filter=tree:0 https://github.com/0verread/cab temp_dir
-cd temp_dir
-git sparse-checkout set --no-cone flask-app
-git checkout
-rm -rf .git
-cd ..
-cp -r temp_dir/ .
-rm -rf temp_dir
+gum spin --spinner dot --title "creating project..." -- git clone -n --depth=1 --filter=tree:0 https://github.com/0verread/cab temp_dir -q;\
+                                                        cd temp_dir;\
+                                                        git sparse-checkout set --no-cone flask-app;\
+                                                        git checkout &> /dev/null;\
+                                                        rm -rf .git;\
+                                                        cd ..;\
+                                                        mkdir "$PROJECT_NAME";\
+                                                        cp -r temp_dir/flask-app/ "$PROJECT_NAME";\
+                                                        rm -rf temp_dir;
 
-echo "[ `date` ]": "$(gum style --foreground 212 END)"
-
+echo "finished creating project $(gum style --foreground "#04B575" "$PROJECT_NAME")"
